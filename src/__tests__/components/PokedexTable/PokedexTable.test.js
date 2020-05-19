344eb691df094e7f8d029ee2035e45f5c5dc3518
igarcia <ivan.garcia@crowdflower.com>
2020-05-19 18:07:05 -0500

    Making tests pass

--- src/__tests__/components/PokedexTable/PokedexTable.test.js
+++ src/__tests__/components/PokedexTable/PokedexTable.test.js
@@ -5,10 +5,13 @@ import PokedexTable from '../../../components/PokedexTable/PokedexTable'
 
 test('Pikachu is displayed', () => {
   const pokemonData = [{
-    name: 'Pikachu'
+    name: 'Pikachu',
+    types: [{"name": "Electric", "color": "rgb(255,255,0)"}, {"name": "Normal", "color": "RGB(255, 192, 203)"}],
+    number: "25",
+    height: "1'04",
+    weight: "13.2 lbs."
   }];
 
   const {getByText} = render(<PokedexTable pokemonData={pokemonData}/>);
-  getByText('Pikachu').toBeInTheDocument();
+  expect(getByText('Pikachu')).toBeInTheDocument();
 });
-
